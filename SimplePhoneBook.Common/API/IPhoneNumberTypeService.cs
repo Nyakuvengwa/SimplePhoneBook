33507c25f536c8669fdcc116e4c80f4c7eacb8ee
arthur.moyo <arthur.moyo@dariel.co.za>
2021-01-27 22:54:10 +0200

    Added the Common Project which is used to hold the services

---
+++ SimplePhoneBook.Common/API/IPhoneNumberTypeService.cs
@@ -0,0 +1,15 @@
+﻿using SimplePhoneBook.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePhoneBook.Common.API
+{
+    public interface IPhoneNumberTypeService
+    {
+        public Task<List<PhoneNumberType>> GetAllPhoneNumberTypes();
+
+        public Task<PhoneNumberType> GetPhoneNumberTypeById(int id);
+    }
+}
---
+++ SimplePhoneBook.Common/Service/PhoneNumberTypeService.cs
@@ -0,0 +1,29 @@
+﻿using SimplePhoneBook.Common.API;
+using SimplePhoneBook.Data.Entites;
+using SimplePhoneBook.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePhoneBook.Common.Service
+{
+    public class PhoneNumberTypeService : IPhoneNumberTypeService
+    {
+        private readonly IPhoneNumberTypeRepository _repository;
+
+        public PhoneNumberTypeService(IPhoneNumberTypeRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<PhoneNumberType>> GetAllPhoneNumberTypes()
+        {
+            return await _repository.GetAllPhoneNumberTypes();
+        }
+
+        public async Task<PhoneNumberType> GetPhoneNumberTypeById(int id)
+        {
+            return await _repository.GetPhoneNumberTypeById(id);
+        }
+    }
+}
